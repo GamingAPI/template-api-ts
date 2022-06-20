5a3e82ed419a436cb7cc877116d980f60cea6be8
Jonas Lagoni <jonas-lt@live.dk>
2022-06-20 16:09:29 +0200

    fix: generate script not working with multiple commit messages

--- generate.sh
+++ generate.sh
@@ -16,68 +16,77 @@ major_version_change="false"
 minor_version_change="false"
 patch_version_change="false"
 commit_message=""
-
 document_last_version=$(cat ./configs.json | jq -r '.document_last_version')
 template_last_version=$(cat ./configs.json | jq -r '.template_last_version')
 library_last_version=$(cat ./package.json | jq -r '.version')
-
-[ -d "./definitions" ] && rm -rf ./definitions
+url_to_asyncapi_document="https://raw.githubusercontent.com/GamingAPI/definitions/main/bundled/<<[ .cus.ASYNCAPI_FILE ]>>"
 
 template_current_version=$(curl -sL https://api.github.com/repos/asyncapi/ts-nats-template/releases/latest | jq -r '.tag_name' | sed 's/v//')
+document_current_version=$(curl -sL ${url_to_asyncapi_document} | jq -r '.info.version' | sed 's/v//')
 
-git clone https://github.com/GamingAPI/definitions.git definitions
-document_current_version=$(cat ./definitions/bundled/<<[ .cus.ASYNCAPI_FILE ]>> | jq -r '.info.version' | sed 's/v//')
-
-semver_template_last_version=( ${template_last_version//./ } )
+# Split the last used template version by '.' to split it up into 'major.minor.fix'
+semver_template_last_version=${template_last_version//./}
 major_template_last_version=${semver_template_last_version[0]}
 minor_template_last_version=${semver_template_last_version[1]}
 patch_template_last_version=${semver_template_last_version[2]}
-semver_template_current_version=( ${template_current_version//./ } )
+# Split the current template version by '.' to split it up into 'major.minor.fix'
+semver_template_current_version=${template_current_version//./ }
 major_template_current_version=${semver_template_current_version[0]}
 minor_template_current_version=${semver_template_current_version[1]}
 patch_template_current_version=${semver_template_current_version[2]}
+if [[ $major_template_current_version > $major_template_last_version ]]; then major_template_change="true"; else major_template_change="false"; fi
+if [[ $minor_template_current_version > $minor_template_last_version ]]; then minor_template_change="true"; else minor_template_change="false"; fi
+if [[ $patch_template_current_version > $patch_template_last_version ]]; then patch_template_change="true"; else patch_template_change="false"; fi
 
-if (($major_template_current_version > $major_template_last_version)); then
-  major_version_change="true"
-  commit_message="${commit_message}Template have changed to a new major version."
-elif (($minor_template_current_version > $minor_template_last_version)); then
-  minor_version_change="true"
-  commit_message="${commit_message}Template have changed to a new minor version."
-elif (($patch_template_current_version > $patch_template_last_version)) && (($minor_template_current_version < $minor_template_last_version)) && (($major_template_current_version < $major_template_last_version)); then
-  patch_version_change="true"
-  commit_message="${commit_message}Template have changed to a new patch version."
-fi
-
-semver_document_last_version=( ${document_last_version//./ } )
+# Split the last used AsyncAPI document version by '.' to split it up into 'major.minor.fix'
+semver_document_last_version=${document_last_version//./}
 major_document_last_version=${semver_document_last_version[0]}
 minor_document_last_version=${semver_document_last_version[1]}
 patch_document_last_version=${semver_document_last_version[2]}
-semver_document_current_version=( ${document_current_version//./ } )
+# Split the current AsyncAPI document version by '.' to split it up into 'major.minor.fix'
+semver_document_current_version=${document_current_version//./}
 major_document_current_version=${semver_document_current_version[0]}
 minor_document_current_version=${semver_document_current_version[1]}
 patch_document_current_version=${semver_document_current_version[2]}
+if [[ $major_document_current_version > $major_document_last_version ]]; then major_document_change="true"; else major_document_change="false"; fi
+if [[ $minor_document_current_version > $minor_document_last_version ]]; then minor_document_change="true"; else minor_document_change="false"; fi
+if [[ $patch_document_current_version > $patch_document_last_version ]]; then patch_document_change="true"; else patch_document_change="false"; fi
 
-if (($major_document_current_version > $major_document_last_version)); then
-  major_version_change="true"
+# Set the commit messages that details what changed
+if (($major_template_change == 'true')); then
+  commit_message="Template have changed to a new major version."
+elif (($minor_template_change == 'true')); then
+  commit_message="Template have changed to a new minor version."
+elif (($patch_template_change == 'true')); then
+  commit_message="Template have changed to a new patch version."
+fi
+if (($major_document_change == 'true')); then
   commit_message="${commit_message}AsyncAPI document have changed to a new major version."
-elif (($minor_document_current_version > $minor_document_last_version)); then
-  minor_version_change="true"
+elif (($minor_document_change == 'true')); then
   commit_message="${commit_message}AsyncAPI document have changed to a new minor version."
-elif (($patch_document_current_version > $patch_document_last_version && $minor_document_current_version < $minor_document_last_version && $major_document_current_version < $major_document_last_version)); then
-  patch_version_change="true"
+elif (($patch_document_change == 'true')); then
   commit_message="${commit_message}AsyncAPI document have changed to a new patch version."
+fi
+
+# Always use the most aggressive version change, and only do one type of version change
+if (($major_template_change == 'true' || $major_document_change == 'true')); then
+  major_version_change="true"
+elif (($minor_template_change == 'true' || $minor_document_change == 'true')); then
+  minor_version_change="true"
+elif (($patch_template_change == 'true' || $patch_document_change == 'true')); then
+  patch_version_change="true"
 fi
 
 if $major_version_change == 'true' || $minor_version_change == 'true' || $patch_version_change == 'true'; then
   # Remove all generated files to ensure clean slate
-  find . -not \( -name configs.json -or -name .gitignore -or -name LICENSE -or -name custom_package.json -or -name generate.sh -or -iwholename *.github* -or -iwholename *./definitions* -or -iwholename *.git* -or -name . \) -exec rm -rf {} +
+  find . -not \( -name configs.json -or -name .gitignore -or -name LICENSE -or -name custom_package.json -or -name generate.sh -or -iwholename *.github* -or -iwholename *.git* -or -name . \) -exec rm -rf {} +
 
   if ! command -v ag &> /dev/null
   then
     npm install -g @asyncapi/generator
   fi
   # Generating code from the AsyncAPI document
-  ag --force-write --output ./ ./definitions/bundled/<<[ .cus.ASYNCAPI_FILE ]>> @asyncapi/ts-nats-template
+  ag --force-write --output ./ ${url_to_asyncapi_document} @asyncapi/ts-nats-template
 
   # Write new config file to ensure we keep the new state for next time
   contents="$(jq ".template_last_version = \"$template_current_version\" | .document_last_version = \"$document_current_version\"" configs.json)" && echo "${contents}" > configs.json
@@ -94,5 +103,5 @@ echo "
 major_version_change="$major_version_change"
 minor_version_change="$minor_version_change"
 patch_version_change="$patch_version_change"
+commit_message="$commit_message"
 " > ./.github/variables/generator.env
-rm -rf ./definitions
